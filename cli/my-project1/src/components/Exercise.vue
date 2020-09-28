52ffadfbf33bed392a38809acc2acad660e5b242
shinnara7 <nainglinucsy@gmail.com>
2020-09-29 01:55:56 +0630

    Vue cli and Vue Router

---
+++ cli/my-project1/src/components/Exercise.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="exercise">
+		<h2 class="text-danger">This is a Exercise component</h2>
+	</div>
+</template>
+<script type="text/javascript">
+	
+</script>
+<style type="text/css">
+	
+</style>
